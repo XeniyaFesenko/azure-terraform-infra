72fa52fa5162e62a91216b7b68a9f22f4548e475
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2024-07-19 16:55:58 -0400

    Update main.tf

--- aci/main.tf
+++ aci/main.tf
@@ -1,3 +1,20 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
