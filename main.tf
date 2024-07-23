5a9b1bedff3d8240aa20311a7d148221965a2081
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2024-07-22 22:50:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,58 @@
-# ARM provider block -rekhu
-provider "azurerm" {
-  version = "~>2.0"
-  features {}
-}
-# Terraform backend configuration block -precreated
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+# Terraform backend configuration block -precreated
   backend "azurerm" {
     resource_group_name  = "poc"
     storage_account_name = "xentfs"
     container_name       = "xentfs-container"
     key                  = "xentfs-container.tfstate"
   }
+}
+
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+## Azure RG
+resource "azurerm_resource_group" "myrgcloudquickpocs" {
+  name     = "cloudquickpocsrg001"
+  location = "East US"
+}
+
+## Azure Logic App
+resource "azurerm_logic_app_workflow" "cloudquickpocslogicapp" {
+  name                = "cloudquickpoclogicapp1"
+  location            = azurerm_resource_group.myrgcloudquickpocs.location
+  resource_group_name = azurerm_resource_group.myrgcloudquickpocs.name
+}
+
+## A custom action in logic app
+resource "azurerm_logic_app_action_custom" "cloudquickpocslogicappcustomaction" {
+  name         = "cloudquickpoclogicapp1-Custom-action-1"
+  logic_app_id = azurerm_logic_app_workflow.cloudquickpocslogicapp.id
+
+  body = <<BODY
+{
+    "description": "A variable to configure the auto expiration age in days. Configured in negative number. Default is -30 (30 days old).",
+    "inputs": {
+        "variables": [
+            {
+                "name": "ExpirationAgeInDays",
+                "type": "Integer",
+                "value": -30
+            }
+        ]
+    },
+    "runAfter": {},
+    "type": "InitializeVariable"
+}
+BODY
+
 }
